8848a27ce68383dc7d2e3cf0f1fab253ff0e1109
takrim1999 <58911305+takrim1999@users.noreply.github.com>
2020-09-23 22:27:20 +0600

    Add files via upload

---
+++ connection.py
@@ -0,0 +1,13 @@
+import requests
+from playsound import playsound
+print("checking wifi...")
+while True:
+    try:
+        if requests.get("http://google.com" , timeout = (10,10)).status_code == 200:
+            print("wifi connected")
+            playsound("wifi.mp3")
+            break
+        else:
+            print("server error")
+    except:
+        print("not connected")
